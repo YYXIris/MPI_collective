abefb8b1a38f7c75a8e7fd3529b3da9b2cd19764
YuxinYeIris <yye@g.harvard.edu>
2024-03-20 10:11:13 -0400

    upload implementation

---
+++ src/mpi_allToAll.cpp
@@ -0,0 +1,30 @@
+#include "mpi.h"
+
+EXTERN_C MPI_METHOD MPI_Alltoall(
+    const void* sendbuf,
+    int sendcount,
+    MPI_Datatype sendtype,
+    void* recvbuf,
+    int recvcount,
+    MPI_Datatype recvtype,
+    MPI_Comm comm
+    ){
+        int size, rank;
+        MPI_RESULT mpi_errno = MPI_Comm_size (comm, &size) || MPI_Comm_rank(comm, &rank);
+
+        if (mpi_errno != MPI_SUCCESS){
+                return mpi_errno;
+        }
+
+        for (int i=0;i<size;i++){
+            mpi_errno=MPI_Scatter(sendbuf, sendcount, sendtype, recvbuf, recvcount, recvtype, i, comm);
+            if (mpi_errno != MPI_SUCCESS){
+                return mpi_errno;
+            }
+            if(i==rank){
+                std::copy(sendbuf, sendbuf+i*sendcount, recvbuf+i*recvcount);
+            }
+        }
+
+        return mpi_errno;
+    }
